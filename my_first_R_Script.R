0cbf97be2fbba6c507d4a910fad0094b629d66e7
SNadezhdaG <naducikk@gmail.com>
2021-02-20 10:40:06 +0700

    User2 chanched my_script

--- my_first_R_Script.R
+++ my_first_R_Script.R
@@ -1,7 +1,11 @@
 example<-matrix(c(1,2,3,4,5,6,7,8),nrow = 4,ncol = 2)
-
+ 
 
 a<-file("hw1_data.csv")
 
-for (i in 1:10){
-print(x[i])}
+g<-function(x){
+  
+  for (i in 1:10)
+    {x<-x+i
+print(x[i])}}
+
