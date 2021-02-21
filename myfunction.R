0d6adb70c868d45b70f3fa00d802d142ff3e2b3c
SNadezhdaG <naducikk@gmail.com>
2021-02-21 19:43:47 +0700

    change Hi, by User2

--- myfunction.R
+++ myfunction.R
@@ -10,5 +10,6 @@ y<-1:20
 z<-seq(1,30)
 
 telegram <- function(...){
-  paste("Hi!", ...,"Bye!"  ,sep = " ", collapse = NULL ) #addfeach1
+  paste("Hi,","there!" ...,"Bye!"  ,sep = " ", collapse = NULL ) #addfeach1
+  
 }
